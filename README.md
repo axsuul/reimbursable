54d63d0aeec2f4bd4027598a8c0ee2a9c15bd5b1
James Hu <hello@james.hu>
2014-04-01 21:58:22 -0700

    Use an ORM, basic functionality

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ reimbursable
 ============
 
 DataNitro script that helps sort reimbursements from Mint.com transactions
+
+## Installation
+
+- Install [peewee](http://peewee.readthedocs.org/) with `pip install peewee`
--- reimbursable.py
+++ reimbursable.py
@@ -1,110 +1,174 @@
 import locale
 import re
-import sqlite3
+import os
+from peewee import *
 
 locale.setlocale(locale.LC_ALL, '')
 
+# Config
+sqlite_file = 'reimbursable.db'
+output_file = 'output.xls'
+sqlite_path = os.path.join(os.path.dirname(__file__), sqlite_file)
+output_path = os.path.join(os.path.dirname(__file__), output_file)
+
 # Input
-inputAmountColumn = "D"
-inputCategoryColumn = "F"
-inputTypeColumn = "E"
-inputLabelColumn = "H"
-inputReimbursableColumn = "I"
+input_amount_column = "D"
+input_category_column = "F"
+input_type_column = "E"
+input_label_column = "H"
+input_reimbursable_column = "J"
 
-# Output
-outputSheet = "Totals"
+output_column = 1
 
-# Clear sheet
-clear_sheet(outputSheet)
+db = SqliteDatabase(sqlite_path)
 
-outputColumn = 1
-grandTotals = {}
-reimbursementTotals = {}
+# Models
+class Reimbursable(Model):
+    name = CharField(index=True)
 
-# Our own data structure that stores everything
-data = {}
+    class Meta:
+        database = db
 
-# Helper to output totals
-def output_totals(name, totals, row, column):
-    total = 0.00
-    headerRow = row
-    row += 1    # make room for header
+class Category(Model):
+    name = CharField(index=True)
 
-    for category, amount in iter(sorted(totals.iteritems())):
-        # Skip categories with 0
-        if amount == 0:
-            continue
+    class Meta:
+        database = db
 
-        categoryCell = Cell(outputSheet, row, column)
-        amountCell = Cell(outputSheet, row, column + 1)
+class Account(Model):
+    name = CharField(index=True)
 
-        categoryCell.value = category
-        amountCell.value = locale.currency(amount)
+    class Meta:
+        database = db
 
-        total += amount
-        row += 1
+class Transaction(Model):
+    reimbursable = ForeignKeyField(Reimbursable, related_name='transactions', null=True)
+    category = ForeignKeyField(Category, related_name='transactions')
+    account = ForeignKeyField(Account, related_name='transactions')
+    date = DateField(index=True, null=True)
+    amount = FloatField()
+    row = IntegerField()
 
-    # Header
-    nameCell = Cell(outputSheet, headerRow, column)
-    nameCell.font.bold = True
-    nameCell.value = name
-    totalCell = Cell(outputSheet, headerRow, column + 1)
-    totalCell.font.bold = True
-    totalCell.value = locale.currency(total)
+    class Meta:
+        database = db
+        # indexes = [(('date', 'row'), False)]
 
-    return row
+# Setup sqlite db if not found
+if not os.path.isfile(sqlite_path):
+    # Create tables
+    Reimbursable.create_table()
+    Category.create_table()
+    Account.create_table()
+    Transaction.create_table()
 
-outputRow = 1
+with db.transaction():
+    for input_sheet in all_sheets():
+        # Skip header
+        input_row = 2
 
-for inputSheet in all_sheets():
-    # Skip output sheet
-    if inputSheet == outputSheet:
-        continue
+        account_name = input_sheet
+        account = Account.get_or_create(name=account_name)
 
-    totals = {}
+        while True:
+            amount_cell = Cell(input_sheet, "{0}{1}".format(input_amount_column, input_row))
+            category_cell = Cell(input_sheet, "{0}{1}".format(input_category_column, input_row))
+            type_cell = Cell(input_sheet, "{0}{1}".format(input_type_column, input_row))
+            label_cell = Cell(input_sheet, "{0}{1}".format(input_label_column, input_row))
+            reimbursable_cell = Cell(input_sheet, "{0}{1}".format(input_reimbursable_column, input_row))
 
-    inputRow = 2
+            # Stop when meet empty rows
+            if amount_cell.is_empty():
+                break
 
-    while True:
-        amountCell = Cell(inputSheet, "{0}{1}".format(inputAmountColumn, inputRow))
-        categoryCell = Cell(inputSheet, "{0}{1}".format(inputCategoryColumn, inputRow))
-        typeCell = Cell(inputSheet, "{0}{1}".format(inputTypeColumn, inputRow))
-        labelCell = Cell(inputSheet, "{0}{1}".format(inputLabelColumn, inputRow))
-        reimbursableCell = Cell(inputSheet, "{0}{1}".format(inputReimbursableColumn, inputRow))
+            amount = float(amount_cell.value)
+            category_name = category_cell.value
+            reimbursable_name = reimbursable_cell.value
+            type = type_cell.value
+            reimbursable = None
 
-        # Stop when meet empty rows
-        if amountCell.is_empty():
-            break
-
-        amount = float(amountCell.value)
-        category = categoryCell.value
-        type = typeCell.value
-        reimbursable = False
-
-        if reimbursableCell.value:
-            reimbursable = True
-
-        if category not in totals:
-            totals[category] = 0.00
-
-        if category not in grandTotals:
-            grandTotals[category] = 0.00
-
-        if reimbursable:
+            # Make amount negative if credit
             if type == "credit":
                 amount = -1*amount
 
-            totals[category] += amount
-            grandTotals[category] += amount
+            try:
+                # If transaction exists in db, only update if
+                # reimbursable is different
+                transaction = Transaction.get(account=account, row=input_row)
 
-        # Next row
-        inputRow += 1
+            except Transaction.DoesNotExist:
+                # If this transaction is reimbursable
+                if reimbursable_name:
+                    reimbursable = Reimbursable.get_or_create(name=reimbursable_name.title())
 
-    outputRow = output_totals(inputSheet, totals, outputRow, 5)
+                category = Category.get_or_create(name=category_name)
+                transaction = Transaction.create(reimbursable=reimbursable, category=category, account=account, amount=amount, row=input_row)
 
-    # Skip for the next sheet
-    outputRow += 1
+            if transaction.reimbursable:
+                if not transaction.reimbursable.name == reimbursable_name:
+                    transaction.reimbursable = reimbursable
+                    transaction.save
 
+            print "Imported transaction #" + str(transaction.id)
 
+            # Next row
+            input_row += 1
 
-output_totals("Totals", grandTotals, 1, 1)
+# Remove old output
+try:
+    os.remove(output_path)
+except OSError:
+    pass
+
+# Create new workbook
+active_wkbk(new_wkbk())
+
+# Now display it all
+for reimbursable in Reimbursable.select():
+    output_sheet = reimbursable.name
+    new_sheet(output_sheet)
+
+    totals = {}
+
+    for transaction in reimbursable.transactions:
+        category = transaction.category
+
+        if category.name not in totals:
+            totals[category.name] = 0.00
+
+        totals[category.name] += transaction.amount
+
+    row = 2
+    grand_total = 0
+
+    for category_name, total in iter(sorted(totals.iteritems())):
+        # Skip categories with 0
+        if total == 0:
+            continue
+
+        category_cell = Cell(output_sheet, row, 1)
+        total_cell = Cell(output_sheet, row, 2)
+
+        category_cell.value = category_name
+        total_cell.value = locale.currency(total)
+
+        grand_total += total
+        row += 1
+
+    grand_total_label_cell = Cell(output_sheet, 1, 1)
+    grand_total_label_cell.value = "Total"
+    grand_total_label_cell.font.bold = True
+
+    grand_total_cell = Cell(output_sheet, 1, 2)
+    grand_total_cell.value = locale.currency(grand_total)
+    grand_total_cell.font.bold = True
+
+    # Autofit sheet cells
+    autofit(output_sheet)
+
+# Remove default sheets
+for sheet in all_sheets():
+    if re.match(r'Sheet', sheet):
+        remove_sheet(sheet)
+
+# Save output
+save(output_path)
